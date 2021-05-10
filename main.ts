5d23de55bbe8b3e34756e574e80c56063c03d9cc
omayraortiz <83971482+omayraortiz@users.noreply.github.com>
2021-05-10 10:57:33 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showString("Historia Laboral")
+    basic.showString("Omayra Ortiz")
+    basic.showString("Docente")
+    basic.showString("Años de servicio")
+    basic.showNumber(49)
+    basic.showString("Maestra,Licenciada, Magister")
+})
